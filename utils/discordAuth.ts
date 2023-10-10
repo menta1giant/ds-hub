a987d15d86610d1508b1641e35c6ada6f912df22
Mikhail Gostev <he.is.really.experienced@gmail.com>
2023-10-10 16:33:30 +0300

    fix spaces in auth link

--- utils/discordAuth.ts
+++ utils/discordAuth.ts
@@ -4,12 +4,9 @@ const PROPS = {
   client_id: '1161273721026261115',
   redirect_uri: REDIRECT_URL,
   response_type: 'code',
-  scope: 'identify'
+  scope: 'identify guilds'
 }
 
-const getAuthLink = () => 'https://discord.com/api/oauth2/authorize?' + new URLSearchParams(Object.entries(PROPS))
-//https://discord.com/api/oauth2/authorize?client_id=1161273721026261115&redirect_uri=https%3A%2F%2Fds-hub.vercel.app%2Fauth%2Fdiscord&response_type=code&scope=identify+guilds
-//https://discord.com/api/oauth2/authorize?client_id=1161273721026261115&redirect_uri=https%3A%2F%2Fds-hub.vercel.app%2Fauth%2Fdiscord&response_type=code&scope=identify%20guilds
-//https://discord.com/api/oauth2/authorize?client_id=1161273721026261115&redirect_uri=https%3A%2F%2Fds-hub.vercel.app%2Fauth%2Fdiscord&response_type=code&scope=identify%2520guilds
+const getAuthLink = () => 'https://discord.com/api/oauth2/authorize?' + (new URLSearchParams(Object.entries(PROPS))).toString().replace("+", "%20")
 
 export default getAuthLink
