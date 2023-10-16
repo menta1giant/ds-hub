bd36b07bdc6024a26e85a5d1f03c7a4f88fc8ea7
Mikhail Gostev <he.is.really.experienced@gmail.com>
2023-10-16 21:08:28 +0300

    test

--- server/api/event.post.ts
+++ server/api/event.post.ts
@@ -5,6 +5,7 @@ export default defineEventHandler(async (event) => {
   protectRoute(event);
 
   const body = await readBody(event);
+  console.log(body);
 
   const {
     name: title,
@@ -13,7 +14,6 @@ export default defineEventHandler(async (event) => {
     description,
     id,
   } = body.event;
-  console.log(body);
 
   return prisma.event.create({
     data: {
